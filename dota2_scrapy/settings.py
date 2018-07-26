c6c455859e73ea7b90cecfe4ff0b387b8e7afc1e
1449902124@qq.com <1449902124@qq.com>
2018-07-26 18:48:01 +0800

    英文改成中文

--- dota2_scrapy/settings.py
+++ dota2_scrapy/settings.py
@@ -22,7 +22,7 @@ CONCURRENT_REQUESTS_PER_IP = 16
 DEFAULT_REQUEST_HEADERS = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36',
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
-    'Accept-Language': 'en'
+    'Accept-Language': 'zh-CN,zh;q=0.9'
 }
 
 RETRY_TIMES = 10
